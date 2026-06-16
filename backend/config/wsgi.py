02e4daeb9c127f8dcced3a135f81fea69168030b
Yi$€£ <yigraciel@hotmail.com>
2026-06-16 11:00:23 -0300

    Set up WSGI for Django application

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -1,1 +1,6 @@
- # Django project settings
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+application = get_wsgi_application()
