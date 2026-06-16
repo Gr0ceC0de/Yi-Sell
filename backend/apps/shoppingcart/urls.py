8fb7231e7191942bafaeaa44a26ab31d5ad423e7
Yi$€£ <yigraciel@hotmail.com>
2026-06-16 10:44:38 -0300

    Update urls.py

--- backend/apps/shoppingcart/urls.py
+++ backend/apps/shoppingcart/urls.py
@@ -6,3 +6,12 @@ app_name = 'shoppingcart'
 urlpatterns = [
     path('', views.index, name='index'),
 ]
+from django.urls import path
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Yi$el Shopping Cart OK!")
+
+urlpatterns = [
+    path('', index, name='index'),
+]
